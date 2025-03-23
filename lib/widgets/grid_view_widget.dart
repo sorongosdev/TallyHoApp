3ef90806ff6402010b7e81d856c84ad948dba057
Sorongos <dosora9@gmail.com>
2025-03-23 10:51:47 +0900

    feat: 배경 이미지에 맞게 그리드뷰 마진 지정

--- lib/widgets/grid_view_widget.dart
+++ lib/widgets/grid_view_widget.dart
@@ -6,24 +6,29 @@ import 'grid_item_widget.dart';
 
 class GridViewWidget extends StatelessWidget {
   final double cellSize;
+  final EdgeInsets margin;
 
   const GridViewWidget({
     super.key,
     required this.cellSize,
+    this.margin = const EdgeInsets.all(14.0), // 기본 외곽 마진 값
   });
 
   @override
   Widget build(BuildContext context) {
     final gridBloc = Provider.of<GridBloc>(context);
     
-    return GridView.count(
-      physics: const NeverScrollableScrollPhysics(), // 스크롤 비활성화
-      crossAxisCount: gridBloc.columns, // BLoC에서 열 개수 가져오기
-      childAspectRatio: 1.0, // 정사각형 비율
-      padding: EdgeInsets.zero, // 패딩 제거
-      children: List.generate(gridBloc.itemCount, (index) {
-        return GridItemWidget(index: index);
-      }),
+    return Container(
+      margin: margin,
+      child: GridView.count(
+        physics: const NeverScrollableScrollPhysics(), // 스크롤 비활성화
+        crossAxisCount: gridBloc.columns, // BLoC에서 열 개수 가져오기
+        childAspectRatio: 1.0, // 정사각형 비율
+        padding: EdgeInsets.zero, // 패딩 제거
+        children: List.generate(gridBloc.itemCount, (index) {
+          return GridItemWidget(index: index);
+        }),
+      ),
     );
   }
 }

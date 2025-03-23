37eb9863dcbd1cd1c4db970b08a7f5ace58138db
Sorongos <dosora9@gmail.com>
2025-03-23 12:07:36 +0900

    feat: 카드 뒷면 기본으로 보이게

--- lib/widgets/grid_item_widget.dart
+++ lib/widgets/grid_item_widget.dart
@@ -6,31 +6,35 @@ import '../bloc/grid_bloc.dart';
 class GridItemWidget extends StatelessWidget {
   final int index;
 
-  const GridItemWidget({
-    super.key, 
-    required this.index,
-  });
+  const GridItemWidget({super.key, required this.index});
 
   @override
   Widget build(BuildContext context) {
     final gridBloc = Provider.of<GridBloc>(context);
     final isSelected = gridBloc.selectedIndex == index;
-    
+
     return GestureDetector(
       onTap: () {
         gridBloc.selectItem(index);
       },
       child: Container(
         decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/card_back.png'),
+            fit: BoxFit.cover,
+          ),
           border: Border.all(color: Colors.black, width: 1.0),
-          color: isSelected ? const Color.fromARGB(255, 136, 174, 191) : Colors.transparent,
+          color:
+              isSelected
+                  ? const Color.fromARGB(255, 136, 174, 191)
+                  : Colors.transparent,
         ),
         child: Center(
           child: Text(
             gridBloc.getItemText(index),
             style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-              fontSize: 20.0,
-              color: isSelected ? Colors.blue[800] : Colors.black,
+              fontSize: 18.0,
+              color: Colors.black,
             ),
           ),
         ),

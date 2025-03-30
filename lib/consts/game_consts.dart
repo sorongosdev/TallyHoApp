6a6013bf0f6395be6015b8b76cfcc40b75f70fe6
Sorongos <dosora9@gmail.com>
2025-03-30 17:30:25 +0900

    refactor: 칸수 관련 상수로 분리

---
+++ lib/consts/game_consts.dart
@@ -0,0 +1,9 @@
+class GameConsts {
+  // 클래스를 인스턴스화하지 못하도록 private 생성자 사용
+  GameConsts._();
+  
+  // 그리드 관련 상수
+  static const int gridRows = 7;
+  static const int gridColumns = 7;
+  static const int gridItemCount = 49;
+}
